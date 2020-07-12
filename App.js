195b98668bfdb02715d6cb79150626d147a4e8c8
RYLiang18 <richard.y.liang@gmail.com>
2020-07-11 20:15:49 -0400

    implemented function to fetch from NPS API + implemented loading screen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   View,
   StyleSheet,
+  ActivityIndicator,
   Text
 } from 'react-native';
 
@@ -10,23 +11,64 @@ import Header from './components/Header'
 import Map from './components/Map'
 import Parks from './components/Parks'
 
-const App = () => {
-  return (
-    <View style={styles.flexContainer}>
-      <View style={[styles.header, styles.component]} >
-        <Header />
-      </View>
-      <View style={[styles.map, styles.component]}>
-        <Map />
-      </View>
-      <View style={[styles.parks, styles.component]}>
-        <Parks />
-      </View>
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      npsParks: null,
+      isLoading: true
+    }
+  }
+
+  // fetch national park json from nps API
+  componentDidMount() {
+    return fetch('https://developer.nps.gov/api/v1/parks?', {
+      headers: {
+        'X-Api-Key': '8rKnkRfD0qGMrR01JpA8kZwn6c5GEOCUwX06SpFW'
+      }
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          npsParks: responseJson.data
+        });
+      })
+      .catch((error) => {
+        console.log("nps made an insane error");
+        console.log(error)
+      })
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      console.log("loading")
+      return (
+        <View style={loadingStyles.flexContainer}>
+          <Text style={loadingStyles.txt}>Loading...</Text>
+        </View>
+      );
+    } else {
+      console.log("finished loading")
+      return (
+        <View style={styles.flexContainer}>
+          <View style={[styles.header, styles.component]} >
+            <Header />
+          </View>
+          <View style={[styles.map, styles.component]}>
+            <Map />
+          </View>
+          <View style={[styles.parks, styles.component]}>
+            <Parks />
+          </View>
+        </View>
+      )
+    }
+  };
 };
 
 const styles = StyleSheet.create({
+
   flexContainer: {
     flexDirection: 'column',
     justifyContent: 'center',
@@ -50,4 +92,14 @@ const styles = StyleSheet.create({
   }
 });
 
-export default App;
+const loadingStyles = StyleSheet.create({
+  flexContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    flex: 1
+  },
+  txt: {
+    textAlign: "center"
+  }
+})
+
